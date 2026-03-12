0b220bd74114b116ef770a7bfb9dc75dd1b3675c
Akash RE <arunaedwin6279@gmail.com>
2026-03-12 22:39:31 +0530

    Use environment variable for server port

--- main.py
+++ main.py
@@ -244,7 +244,10 @@ async def history(user_id: str):
 # -------------------------
 # Local run
 # -------------------------
-
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+    import os
+
+    port = int(os.environ.get("PORT", 8000))
+
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
